c4f61dc9d25d98a42234656745831afcca7075ac
iamdudumon <kksy4511@naver.com>
2024-10-22 21:05:17 +0900

    feat: 프로그램 종료 핸들러 추가

---
+++ src/end_handler.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   end_handler.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 17:04:14 by dukim             #+#    #+#             */
+/*   Updated: 2024/10/22 17:04:14 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ft_philo.h"
+
+void	handle_philo_end(t_philo philo)
+{
+	uint    i;
+
+	// i = 0;
+	// while (i < nop)
+	// {
+	// 	pthread_mutex_destroy(&philos[0].forks[i]);
+	// 	i++;
+	// }
+	// free(philo.infos);
+	// free(philo.forks);
+}

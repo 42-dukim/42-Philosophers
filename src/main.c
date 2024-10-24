39fd60de027099c5c5494c7d3c51fededb3746d8
iamdudumon <kksy4511@naver.com>
2024-10-24 19:49:51 +0900

    feat: main 문 철학자 루틴 시작 함수 실행

--- src/main.c
+++ src/main.c
@@ -20,16 +20,19 @@ int	main(int argc, char *argv[])
 	if (argc < 5 || argc > 6)
 		return (0);
 	philo.opt = parse_arg_to_philo_opt(argc, argv);
+	// t_philo_opt opt = *(philo.opt);
 	// printf("number_of_philosophers: %u\ntime_to_die: %u\ntime_to_eat: %u\ntime_to_sleep: %u\nnumber_of_times_each_philosopher_must_eat: %u\n", \
 	// 		opt.nop, opt.ttd, opt.tte, opt.tts, opt.nme);
 	philo.forks= create_forks(philo.opt->nop);
 	philo.infos = create_philo_infos(*(philo.opt), philo.forks);
+	if (start_philo_routine(philo))
+		return (0);
 	handle_monitoring(philo);
 
 	i = 0;
 	while (i < philo.opt->nop)
 	{
-		// pthread_detach(philos[i].thread);
+		// pthread_detach(philo.infos[i].thread);
 		pthread_join(philo.infos[i].thread, NULL);
 		i++;
 	}
